5db25c002e545e54e8f612afc86baaf87cf0092d
natalia.sepka <natalia.sepka@gmail.com>
2024-03-29 11:22:15 +0100

    Dynamically updating players log.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
+import Log from "./components/Log.jsx";
+
 function App() {
     const [activePlayer, setActivePlayer] = useState('X');
+    const [gameTurns, setGameTurns] = useState([]);
 
-    function handleSelectSquare() {
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+        setGameTurns(prevTurns => {
+            let currentPlayer = 'X';
+
+            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+                currentPlayer = 'O';
+            }
+            return [
+                {square: {row: rowIndex, col: colIndex}, player: currentPlayer},
+                ...prevTurns
+            ];
+        })
     }
 
   return (
@@ -15,8 +29,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+       <Log turns={gameTurns}/>
    </main>
   )
 }
